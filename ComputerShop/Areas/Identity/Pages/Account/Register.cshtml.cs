6a37e8e91753af506a8d424e9ef40b2870e84ed4
Radosław Gęgotek <rg303195@student.polsl.pl>
2023-04-17 16:01:35 +0200

    Wybieranie roli admin/customer

--- ComputerShop/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ ComputerShop/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -17,7 +17,9 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
 
@@ -102,6 +104,12 @@ namespace ComputerShop.Areas.Identity.Pages.Account
             [Display(Name = "Confirm password")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
+
+
+            public string? Role { get; set; }
+            [ValidateNever]
+            public IEnumerable<SelectListItem> RoleList { get; set; }
+
         }
 
 
@@ -113,6 +121,15 @@ namespace ComputerShop.Areas.Identity.Pages.Account
 				_roleManager.CreateAsync(new IdentityRole(SD.Role_User_Admin)).GetAwaiter().GetResult();
 				_roleManager.CreateAsync(new IdentityRole(SD.Role_User_Customer)).GetAwaiter().GetResult();
 			}
+
+            Input = new InputModel()
+            {
+                RoleList = _roleManager.Roles.Select(x => x.Name).Select(i => new SelectListItem
+                {
+                    Value = i,
+                    Text = i
+                })
+            };
 
 			ReturnUrl = returnUrl;
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
@@ -133,6 +150,16 @@ namespace ComputerShop.Areas.Identity.Pages.Account
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
+
+
+                    if (!String.IsNullOrEmpty(Input.Role))
+                    {
+						await _userManager.AddToRoleAsync(user, Input.Role);
+					}
+                    else
+                    {
+						await _userManager.AddToRoleAsync(user, SD.Role_User_Customer);
+					}
 
                     var userId = await _userManager.GetUserIdAsync(user);
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
