9407c5eee2886b0c74b7081ad03fbb9ab33dc6cb
Radosław Gęgotek <rg303195@student.polsl.pl>
2023-04-22 12:00:07 +0200

    Nav Bar

--- ComputerShop/Controllers/HomeController.cs
+++ ComputerShop/Controllers/HomeController.cs
@@ -12,25 +12,15 @@ namespace ComputerShop.Controllers
         private readonly ApplicationDbContext _data;
 		private readonly ILogger<HomeController> _logger;
 
-		public HomeController(ILogger<HomeController> logger,ApplicationDbContext data)
+		public HomeController(ILogger<HomeController> logger, ApplicationDbContext data)
 		{
 			_data = data;
-			_logger = logger;		
-        }
+			_logger = logger;
+		}
 
 		public IActionResult Index()
 		{
-			List<HomeViewModel> model = new List<HomeViewModel>();
-			foreach (var item in _data.CategoryGroups)
-			{
-				model.Add(new HomeViewModel
-				{
-					CategoryGroup = item,
-					Categories = _data.Categories.Where(x => x.CategoryGroupId == item.Id)
-				});
-			}
-
-			return View(model);
+			return View();
 		}
 
 		public IActionResult Privacy()
---
+++ ComputerShop/Controllers/NavigationBarController.cs
@@ -0,0 +1,30 @@
+﻿using ComputerShop.Data;
+using ComputerShop.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ComputerShop.Controllers
+{
+    public class NavigationBarController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public NavigationBarController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<HomeViewModel> model = new List<HomeViewModel>();
+            foreach (var item in _context.CategoryGroups)
+            {
+                model.Add(new HomeViewModel
+                {
+                    CategoryGroup = item,
+                    Categories = _context.Categories.Where(x => x.CategoryGroupId == item.Id)
+                });
+            }
+
+            return PartialView("_NavigationBarPartial", model);
+        }
+    }
+}
