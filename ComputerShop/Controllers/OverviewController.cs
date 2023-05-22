a092f2cb0cf8fe58310377d9e8c1fe0b03c72337
Kamil Surlas <ks303231@student.polsl.pl>
2023-05-22 17:11:43 +0200

    searching mechanism 1.0

--- ComputerShop/Controllers/OverviewController.cs
+++ ComputerShop/Controllers/OverviewController.cs
@@ -2,6 +2,7 @@
 using ComputerShop.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using System.Diagnostics;
 
 namespace ComputerShop.Controllers
@@ -9,14 +10,24 @@ namespace ComputerShop.Controllers
     public class OverviewController : Controller
     {
         private readonly ApplicationDbContext _context;
+       
         public OverviewController(ApplicationDbContext context)
         {
             _context = context;
         }
         public IActionResult Index(int dataId)
-        {
+        {           
+            var searchingText = HttpContext.Request.Query["searchingText"];
             OverviewPageViewModel overviewPageViewModel = new OverviewPageViewModel();
-            var productsToShow = _context.Products.Include(x=>x.Producer).Where(x => x.CategoryId == dataId).ToList();
+            List<Models.Product> productsToShow;
+            if (!string.IsNullOrEmpty(searchingText))
+            {
+                productsToShow = _context.Products.Include(x => x.Producer).Where(x => x.Name.Contains(searchingText)).ToList();
+            }
+            else
+            {
+                productsToShow = _context.Products.Include(x => x.Producer).Where(x => x.CategoryId == dataId).ToList();
+            }
             foreach (var item in productsToShow)
             {
                 if(item.Producer.IsPromoted == true)
@@ -29,5 +40,6 @@ namespace ComputerShop.Controllers
             }
             return View(overviewPageViewModel);
         }
+        
     }
 }
