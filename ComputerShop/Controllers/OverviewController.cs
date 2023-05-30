0d1819d2dd2c7d5b411f4caabb615ae53f7bdfd0
Kamil Surlas <ks303231@student.polsl.pl>
2023-05-31 00:00:59 +0200

    filters

--- ComputerShop/Controllers/OverviewController.cs
+++ ComputerShop/Controllers/OverviewController.cs
@@ -1,9 +1,12 @@
 ﻿using ComputerShop.Data;
+using ComputerShop.Models;
 using ComputerShop.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using System.Collections.Generic;
 using System.Diagnostics;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace ComputerShop.Controllers
 {
@@ -16,7 +19,8 @@ namespace ComputerShop.Controllers
             _context = context;
         }
         public IActionResult Index(int dataId)
-        {           
+        {
+            ViewData["DataId"] = dataId;
             var searchingText = HttpContext.Request.Query["searchingText"];
             OverviewPageViewModel overviewPageViewModel = new OverviewPageViewModel();
             List<Models.Product> productsToShow;        
@@ -32,15 +36,60 @@ namespace ComputerShop.Controllers
             {
                 if(item.Producer.IsPromoted == true)
                 {
-                    overviewPageViewModel.PromotedProducts.Add(item);
+                    overviewPageViewModel.PromotedProducts.Add(item);                   
                 } else
                 {
                     overviewPageViewModel.Products.Add(item);
                 }
+               
             }
+            var producers = _context.Products.Include(x => x.Producer).Where(x => x.CategoryId == dataId).Select(x => x.Producer).ToList();
+            foreach (var item in producers)
+            {
+                overviewPageViewModel.Producents.Add(item.Name);
+            }
+            overviewPageViewModel.Producents =  overviewPageViewModel.Producents.Distinct().ToList();
             return View(overviewPageViewModel);
             
         }
+        [HttpGet]
+        public IActionResult FilterProducts(List<string>producer, int? minCena, int? maxCena, int dataId)
+        {
+            ViewData["DataId"] = dataId;
+            List<Models.Product> productsToShow = _context.Products.Include(x => x.Producer).Where(x=>x.CategoryId==dataId).ToList();
+            if (producer.Count > 0)
+            {
+                productsToShow = productsToShow.Where(x => producer.Contains(x.Producer.Name)).ToList();
+            }
+            if(minCena != null) {
+                productsToShow = productsToShow.Where(x => x.Price >= minCena).ToList();
+            
+            }
+            if (maxCena != null)
+            {
+                productsToShow = productsToShow.Where(x => x.Price <= maxCena).ToList();
 
+            }         
+            OverviewPageViewModel overviewPageViewModel = new OverviewPageViewModel();
+            foreach (var item in productsToShow)
+            {
+                if (item.Producer.IsPromoted == true)
+                {
+                    overviewPageViewModel.PromotedProducts.Add(item);
+                }
+                else
+                {
+                    overviewPageViewModel.Products.Add(item);
+                }
+            }
+            var productProducers = _context.Products.Include(x => x.Producer).Where(x => x.CategoryId == dataId).Select(x => x.Producer).ToList();
+            foreach (var item in productProducers)
+            {
+                overviewPageViewModel.Producents.Add(item.Name);
+            }
+            overviewPageViewModel.Producents = overviewPageViewModel.Producents.Distinct().ToList();
+            return View("Index",overviewPageViewModel);
+
+        }
     }
 }
