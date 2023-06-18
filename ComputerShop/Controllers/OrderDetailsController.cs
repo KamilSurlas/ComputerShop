6ea2175e51877460228776681cb4335ea1ba39b9
Radosław Gęgotek <radek.gegotek01@gmail.com>
2023-06-18 13:50:32 +0200

    Przegladanie zamowien

---
+++ ComputerShop/Controllers/OrderDetailsController.cs
@@ -0,0 +1,25 @@
+﻿using ComputerShop.Data;
+using ComputerShop.Data.SD;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ComputerShop.Controllers
+{
+    [Authorize(Roles = SD.Role_User_Admin)]
+    public class OrderDetailsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrderDetailsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var orders = _context.OrderDetails.Include(x => x.Order).Include(x => x.Product).ToList();
+            return View(orders);
+        }
+    }
+}
